653e37cf06d8c0d9caf2102eaa1950427a7e3f1a
Student <student@lab-315-001.cs.csubak.edu>
2017-07-18 10:48:37 -0700

    Began moving button

--- MyApplication/app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.student.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AbsoluteLayout;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int buttonPos = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +25,24 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            private int buttonPos = 0;
+
             @Override
             public void onClick(View view) {
-                TextView textBox = (TextView) findViewById(R.id.textview);
-                textBox.setText(textBox.getText() == "Hello World" ? "Christian Russell"
-                    : "Hello World"
-                );
-                Toast.makeText(getApplicationContext(), "Christian Says:\nBurned Bread == Best Toast", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(), "Making toast from a mailbox? sounds like a bad idea", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), String.format("X: %f; Y: %f", view.getX(), view.getY()), Toast.LENGTH_LONG).show();
+                if (buttonPos % 2 == 0) {
+                    System.out.printf("%f", view.getX());
+                    //view.setX(-view.getX();
+                    view.setX(getApplicationContext().getResources().getDisplayMetrics().widthPixels * (2/3));
+                    System.out.printf("%f", view.getX());
+                } else {
+                    System.out.printf("%f", view.getY());
+                    //view.setY(-view.getY());
+                    view.setX(getApplicationContext().getResources().getDisplayMetrics().widthPixels / 3);
+                    System.out.printf("%f", view.getY());
+                }
+                buttonPos ++;
             }
         });
         Button fab2 = (Button)this.findViewById(R.id.fab2);
@@ -49,10 +62,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        TextView textBox = (TextView) findViewById(R.id.textview);
-        textBox.setText(textBox.getText() == "Hello World" ? "Christian Russell"
-                : "Hello World"
-        );
+        //TextView textBox = (TextView) findViewById(R.id.textview);
+        //textBox.setText(textBox.getText() == "Hello World" ? "Christian Russell"
+        //        : "Hello World"
+        //);
         return true;
     }
 
